6ed4b2bfd566d368024a6fe56f99eb555e360feb
itamar <itamar@example.com>
2023-02-06 20:27:40 +0100

    modificaciones finales

--- Itamar_B.py
+++ Itamar_B.py
@@ -1,32 +1,31 @@
 import json
-#Crear una funció que mostri, per consola, i guardi en un arxiu extern,
+# Crear una funció que mostri, per consola, i guardi en un arxiu extern,
 # un JSON amb una key de nom book que contindrà titel, cover, year i pages
 
 def jsonString():
-    with open('booksFile.json', 'w') as file:
-        libros=""" {'Books:
-        
-             [{"title":"la insoportable levedad del Ser",
-                "cover":"verde",
-                "year":"1973",
-                "pages":"458" },
-                
-             {"title":"La Republica",
-                "cover":"Blanca",
-                "year":"237 a.c",
-                "pages":"732" },
-                
-             {"title":"El Quijote",
-                "cover":"moñigo",
-                "year":"1632",
-                "pages":"789" }, 
-                
-             {"title":"El mercader de Venecia",
-                "cover":"Lila",
-                "year":"1633",
-                "pages":"231" },
-              ]}
-    """
+    libros=""" {'Books:
+    
+         [{"title":"la insoportable levedad del Ser",
+            "cover":"verde",
+            "year":"1973",
+            "pages":"458" },
+            
+         {"title":"La Republica",
+            "cover":"Blanca",
+            "year":"237 a.c",
+            "pages":"732" },
+            
+         {"title":"El Quijote",
+            "cover":"moñigo",
+            "year":"1632",
+            "pages":"789" }, 
+            
+         {"title":"El mercader de Venecia",
+            "cover":"Lila",
+            "year":"1633",
+            "pages":"231" },
+          ]}
+        """
     with open ("book1",'w') as file:
         json.dump(libros,file)
 
